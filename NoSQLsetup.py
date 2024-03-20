cae06abc372b8c05e3e6b6beb95f7490a9b5db70
Michael Kollasch <mrkollasch@comcast.net>
2024-03-20 09:52:22 -0500

    minor fixes complete

--- NoSQLsetup.py
+++ NoSQLsetup.py
@@ -1,43 +1,24 @@
-# %% [markdown]
-# # Eat Safe, Love
+## Part 1: Database and Jupyter Notebook Set Up
 
-# %% [markdown]
-# ## Part 1: Database and Jupyter Notebook Set Up
-
-# %% [markdown]
-# Import the data provided in the `establishments.json` file from your Terminal. Name the database `uk_food` and the collection `establishments`.
-# 
-# Within this markdown cell, copy the line of text you used to import the data from your Terminal. This way, future analysts will be able to repeat your process.
-# 
-# e.g.: Import the dataset with  
-# 
+# Import the dataset with  
 # 'mongoimport --type json -d uk_food -c establishments --drop --jsonArray establishments.json'
-# 
 
-# %%
 # Import dependencies
 from pymongo import MongoClient
 from pprint import pprint
 
-# %%
 # Create an instance of MongoClient
 mongo = MongoClient(port=27017)
 
-# %%
 # confirm that our new database was created
-
 print(mongo.list_database_names())
 
-# %%
 # assign the uk_food database to a variable name
 db = mongo.uk_food
 
-
-# %%
 # review the collections in our new database
 print(db.list_collection_names())
 
-# %%
 # review the collections in our new database
 limit = 5
 records = db.establishments.find().limit(limit)
@@ -45,23 +26,18 @@ records = db.establishments.find().limit(limit)
 pprint(list(records))
 
 
-
-# %%
 # review a document in the establishments collection
 records = db.establishments.find_one({'AddressLine3':'London'})
 pprint(records)
 
-# %%
-# assign the collection to a variable
+# assign the collection to a variable named 'establishments'
 establishments = db['establishments']
 
-# %% [markdown]
-# ## Part 2: Update the Database
 
-# %% [markdown]
-# 1. An exciting new halal restaurant just opened in Greenwich, but hasn't been rated yet. The magazine has asked you to include it in your analysis. Add the following restaurant "Penang Flavours" to the database.
+## Part 2: Update the Database
 
-# %%
+# Add the restaurant "Penang Flavours" to the database.
+
 # Create a dictionary for the new restaurant data
 Penang = {
     "BusinessName":"Penang Flavours",
@@ -93,21 +69,15 @@ Penang = {
 }
 
 
-# %%
 # Insert the new restaurant into the collection
 db.establishments.insert_one(Penang)
 
-# %%
 # Check that the new restaurant was inserted
 query = {"BusinessName":"Penang Flavours"} 
 results = establishments.find(query)
 for result in results:
     print(result)
 
-# %% [markdown]
-# 2. Find the BusinessTypeID for "Restaurant/Cafe/Canteen" and return only the `BusinessTypeID` and `BusinessType` fields.
-
-# %%
 # Find the BusinessTypeID for "Restaurant/Cafe/Canteen" and return only the BusinessTypeID and BusinessType fields
 query = {"BusinessTypeID": {"$eq" :1}}
 fields = {"BusinessType":1,"BusinessTypeID": 1}
@@ -115,32 +85,24 @@ limit = 5
 
 results = establishments.find(query,fields).limit(limit)
 
-
 pprint(list(results))
 
 
-# %% [markdown]
-# 3. Update the new restaurant with the `BusinessTypeID` you found.
-
-# %%
 # Update the new restaurant with the correct BusinessTypeID
 filter = {"BusinessName":"Penang Flavours"}
-newvalues = {"$set": {"BusinessTypeID": 1 }
-                                            }
+newvalues = {"$set": {"BusinessTypeID": 1 }}
 
 establishments.update_one(filter, newvalues)
 
-# %%
 # Confirm that the new restaurant was updated
 query = {"BusinessName":"Penang Flavours"} 
 results = establishments.find(query)
 for result in results:
     print(result)
 
-# %% [markdown]
-# 4. The magazine is not interested in any establishments in Dover, so check how many documents contain the Dover Local Authority. Then, remove any establishments within the Dover Local Authority from the database, and check the number of documents to ensure they were deleted.
 
-# %%
+# Remove any establishments within the Dover Local Authority from the database.
+
 # Find how many documents have LocalAuthorityName as "Dover"
 query = {'LocalAuthorityName': 'Dover'}
 
@@ -148,28 +110,21 @@ doc_count = db.establishments.count_documents(query)
 
 print(doc_count)
 
-# %%
 # Delete all documents where LocalAuthorityName is "Dover"
 db.establishments.delete_many(query)
 
-# %%
 # Check if any remaining documents include Dover
 results = establishments.find(query)
 
 pprint(list(results))
 
-# %%
-# Check that other documents remain with 'find_one'
+# Check that other documents remain 
 records = db.establishments.find_one({'AddressLine3':'London'})
 pprint(records)
 
-# %% [markdown]
-# 5. Some of the number values are stored as strings, when they should be stored as numbers.
 
-# %% [markdown]
-# Use `update_many` to convert `latitude` and `longitude` to decimal numbers.
+# Convert `latitude` and `longitude` to decimal numbers.
 
-# %%
 # Change the data type from String to Decimal for longitude and latitude
 new_values = [
     {'$set':{
@@ -183,15 +138,12 @@ new_values = [
         {'$toDouble':"$geocode.longitude"}}}]
 db.establishments.update_many({}, new_values)
 
-# %% [markdown]
-# Use `update_many` to convert `RatingValue` to integer numbers.
+# Convert `RatingValue` to integer numbers.
 
-# %%
 # Set non 1-5 Rating Values to Null
 non_ratings = ["AwaitingInspection", "Awaiting Inspection", "AwaitingPublication", "Pass", "Exempt"]
 establishments.update_many({"RatingValue": {"$in": non_ratings}}, [ {'$set':{ "RatingValue" : None}} ])
 
-# %%
 # Change the data type from String to Integer for RatingValue
 new_values = [
     {'$set':{
@@ -199,7 +151,6 @@ new_values = [
         {'$toInt':"$RatingValue"}}}]
 db.establishments.update_many({}, new_values)
 
-# %%
 # Check that the coordinates and rating value are now numbers
 records = db.establishments.find_one()
 pprint(records)
